16a2e10461e70381135bf39e3b2d62b064bc21d5
Burk Manson <burkmanson@gmail.com>
2021-07-08 00:59:05 -0700

    basic db stuff

--- db/index.js
+++ db/index.js
@@ -1,13 +1,49 @@
 // Connect to DB
-const { Client } = require('pg');
-const DB_NAME = 'change-this-name'
-const DB_URL = process.env.DATABASE_URL || `postgres://${ DB_NAME }`;
+const { Client } = require("pg");
+const DB_NAME = "linkerator";
+const DB_URL = process.env.DATABASE_URL || `postgres://${DB_NAME}`;
 const client = new Client(DB_URL);
 
 // database methods
+async function createLink({ url, clickCount, comment }) {
+  try {
+    const {
+      rows: [link],
+    } = await client.query(
+      `
+      INSERT INTO links(url, clickCount, comment)
+      VALUES($1, $2, $3)
+      RETURNING *;
+    `,
+      [url, clickCount, comment]
+    );
+
+    return link;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTag({ name }) {
+  try {
+    const {
+      rows: [tag],
+    } = await client.query(
+      `
+      INSERT INTO tags(name)
+      VALUES($1)
+      RETURNING name
+    `,
+      [name]
+    );
+  } catch (error) {
+    throw error;
+  }
+}
 
 // export
 module.exports = {
   client,
-  // db methods
-}
+  createLink,
+  createTag,
+};
--- db/init_db.js
+++ db/init_db.js
@@ -1,17 +1,43 @@
 // code to build and initialize DB goes here
 const {
-  client
-  // other db methods 
-} = require('./index');
+  client,
+  createLink,
+  createTag,
+  // other db methods
+} = require("./index");
 
 async function buildTables() {
   try {
-    client.connect();
+    await client.connect();
 
     // drop tables in correct order
+    await client.query(`
+      DROP TABLE IF EXISTS link_tags;
+      DROP TABLE IF EXISTS tags;
+      DROP TABLE IF EXISTS links;
+    `);
 
     // build tables in correct order
+    await client.query(`
+      CREATE TABLE links(
+        id SERIAL PRIMARY KEY,
+        url VARCHAR(255) NOT NULL,
+        clickCount INT NOT NULL,
+        comment VARCHAR(255) NOT NULL,
+        date DATE DEFAULT CURRENT_DATE
+      )
 
+      CREATE TABLE tags(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255)
+      )
+
+      CREATE TABLE link_tags(
+        id SERIAL PRIMARY KEY,
+        "linksId" INT REFERENCES links("id") NOT NULL,
+        "tagsId" INT REFERENCES tags("id") NOT NULL
+      )
+    `);
   } catch (error) {
     throw error;
   }
@@ -20,6 +46,41 @@ async function buildTables() {
 async function populateInitialData() {
   try {
     // create useful starting data
+    const initialLinks = [
+      {
+        url: "https://www.youtube.com/",
+        clickCount: 15,
+        comment: "Oh man I love this site!",
+      },
+      {
+        url: "https://www.twitter.com/",
+        clickCount: 22,
+        comment: "Oh buddy! This is a good one!",
+      },
+      {
+        url: "https://www.ytmnd.com/",
+        clickCount: 3,
+        comment: "I don't believe it!",
+      },
+    ];
+
+    await Promise.all(initialLinks.map(createLink));
+
+    console.log("Links Created");
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function populateInitialTagData() {
+  try {
+    const initialTags = [
+      { name: "Social" },
+      { name: "Funny" },
+      { name: "Epic" },
+    ];
+
+    await Promise.all(initialTags.map(createTag));
   } catch (error) {
     throw error;
   }
@@ -27,5 +88,6 @@ async function populateInitialData() {
 
 buildTables()
   .then(populateInitialData)
+  .then(populateInitialTagData)
   .catch(console.error)
   .finally(() => client.end());
